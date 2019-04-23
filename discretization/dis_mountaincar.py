e6a40340748e717128730c31119fdde61dabfa2c
wpumacay <wpumacay@gmail.com>
2019-04-23 15:00:16 -0500

    [UPDATE]: added tiling agent for mountain car

--- discretization/dis_mountaincar.py
+++ discretization/dis_mountaincar.py
@@ -9,7 +9,55 @@ from IPython.core.debugger import set_trace
 import dis_utils
 import dis_agent
 
-def experimentGridAgent( env ) :
+# define some hyperparameters
+GAMMA = 0.99
+EPSILON = 1.0
+ALPHA = 0.1
+USE_EPSILON_DECAY = True
+USE_ALPHA_DECAY = False
+NUM_EPISODES = 20000
+
+def setupGridAgent( slow, shigh, nactions ) :
+    # number of bins to use for the discretization
+    NUM_BINS = 20
+    _nbins = tuple( NUM_BINS for _ in range( len( slow ) ) )
+    # create the grid-discretization agent
+    _agent = dis_agent.QLearningGridAgent( slow, 
+                                           shigh,
+                                           nactions,
+                                           _nbins,
+                                           GAMMA,
+                                           EPSILON,
+                                           ALPHA,
+                                           USE_ALPHA_DECAY,
+                                           USE_EPSILON_DECAY )
+
+    return _agent
+
+def setupTilingAgent( slow, shigh, nactions ) :
+    # number of bins to use for the discretization
+    NUM_BINS = 20
+    _nbins = tuple( NUM_BINS for _ in range( len( slow ) ) )
+    # offsets for the tilings
+    _offsets = ( shigh - slow ) / ( 3 * NUM_BINS )
+    # tilings specs
+    _tilingSpecs = [ ( _nbins, -_offsets ),
+                     ( _nbins, tuple( 0.0 for _ in range( len( slow ) ) ) ),
+                     ( _nbins, _offsets ) ]
+    # create the tiling-discretization agent
+    _agent = dis_agent.QLearningTilingAgent( slow,
+                                             shigh,
+                                             nactions,
+                                             _tilingSpecs,
+                                             GAMMA,
+                                             EPSILON,
+                                             ALPHA,
+                                             USE_ALPHA_DECAY,
+                                             USE_EPSILON_DECAY )
+
+    return _agent
+
+def experimentDiscretizationAgent( env, agentType ) :
 
     # grab low-high limits from environment
     _slow = env.observation_space.low
@@ -18,27 +66,9 @@ def experimentGridAgent( env ) :
     # grab the number of discrete actions available
     _nactions = env.action_space.n
 
-    # number of bins to use for the discretization
-    _nbins = tuple( 50 for _ in range( len( _slow ) ) )
-
-    # define some hyperparameters
-    GAMMA = 0.99
-    EPSILON = 1.0
-    ALPHA = 0.02
-    USE_EPSILON_DECAY = True
-    USE_ALPHA_DECAY = False
-    NUM_EPISODES = 100000
-
-    # create the grid-discretization agent
-    _agent = dis_agent.QLearningGridAgent( _slow, 
-                                           _shigh,
-                                           _nactions,
-                                           _nbins,
-                                           GAMMA,
-                                           EPSILON,
-                                           ALPHA,
-                                           USE_ALPHA_DECAY,
-                                           USE_EPSILON_DECAY )
+    # create the appropriate agent
+    _agent = setupGridAgent( _slow, _shigh, _nactions ) if agentType == 'grid' else \
+             setupTilingAgent( _slow, _shigh, _nactions )
 
     # start training
     _progressbar = tqdm( range( 1, NUM_EPISODES + 1 ), desc = 'Training:', leave = True )
@@ -86,7 +116,7 @@ def experimentGridAgent( env ) :
 
     _ = input( 'Press any key to continue with testing ...' )
 
-    for _ in range( 5 ) :
+    for _ in range( 10 ) :
 
         _state = env.reset()
 
@@ -102,4 +132,5 @@ def experimentGridAgent( env ) :
             _state = _snext
 
 if __name__ == '__main__' :
-    experimentGridAgent( gym.make( 'MountainCar-v0' ) )
+    ## experimentDiscretizationAgent( gym.make( 'MountainCar-v0' ), 'grid' )
+    experimentDiscretizationAgent( gym.make( 'MountainCar-v0' ), 'tiling' )
